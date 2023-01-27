38e8d408bdc0b9759ed6ee915566adf1292fe9df
Gaurav Makhijani <makhijani.gaurav19@gmail.com>
2023-01-27 22:49:43 +0530

    Initialize DB

---
+++ db/pg.go
@@ -0,0 +1,27 @@
+package db
+
+import (
+	"log"
+
+	"github.com/jmoiron/sqlx"
+	_ "github.com/lib/pq"
+)
+
+const (
+	dbDriver = "postgres"
+	uri      = "postgres://gaurav:4444@localhost:5432/status?sslmode=disable"
+)
+
+var DB *sqlx.DB
+
+func Init() {
+
+	var err error
+
+	DB, err = sqlx.Connect(dbDriver, uri)
+	if err != nil {
+		log.Fatal("Cannot initialize database", err)
+	}
+
+	log.Println("Database initialized")
+}
--- main.go
+++ main.go
@@ -1,16 +1,22 @@
 package main
 
 import (
+	"StatusChecker/db"
 	"fmt"
 	"net/http"
 )
 
 func main() {
 
+	//initialize database
+	db.Init()
+
+	//handle requests
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprint(w, "Hello, World!")
 	})
 
+	//start server
 	fmt.Println("Server starting on http://localhost:8080")
 	http.ListenAndServe(":8080", nil)
 }
